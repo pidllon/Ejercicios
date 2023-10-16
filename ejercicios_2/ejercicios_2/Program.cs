5a8a9e911a08714acdfabba8f73a00676686b57e
pidllon <iago.rodriguez.perez@ciclosmontecastelo.com>
2023-10-16 12:38:38 +0200

    ifelse

---
+++ ejercicios_2/ejercicios_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //programa que a partir del salario bruto de un trabajador aplica un irpf del 15% di el salario 
+            // <= 1200€ y un irpf del 19 % si es superior
+            float salarioBruto, irpf15, irpf19, salarioNeto;
+            Console.WriteLine("introduzca el salario bruto:");
+            salarioBruto = int.Parse(Console.ReadLine());
+            Console.Clear();
+            irpf15 = salarioBruto * 0.15f;
+            irpf19 = salarioBruto * 0.19f;
+            if (salarioBruto <= 1200)
+            {
+                salarioNeto = salarioBruto - irpf15;
+            }
+            else
+            {
+                salarioNeto = salarioBruto - irpf19;
+            }
+            Console.WriteLine("salario neto es " + salarioNeto);
+            Console.ReadLine();
+
+        }
+    }
+}
