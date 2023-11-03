a660f395ae0def58c0a62eee658998a35914abdd
pidllon <iago.rodriguez.perez@ciclosmontecastelo.com>
2023-11-03 12:26:03 +0100

    h

---
+++ ejercicios_3/ejercicios_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bucles 
+        }
+    }
+}
