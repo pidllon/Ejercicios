7b60435fb854de2c1f4a7aed068cd32585ae8352
pidllon <iago.rodriguez.perez@ciclosmontecastelo.com>
2023-11-08 13:06:07 +0100

    h

--- ejercicios_3/ejercicios_3/Program.cs
+++ ejercicios_3/ejercicios_3/Program.cs
@@ -10,7 +10,7 @@ namespace ejercicios_3
     {
         static void Main(string[] args)
         {
-            int a, b;
+            /*int a, b;
             Console.WriteLine("Los números pares saldran en azul y los impares en blanco.");
             Console.WriteLine("Introduce el mínimo:");
             a = int.Parse(Console.ReadLine());
@@ -32,6 +32,39 @@ namespace ejercicios_3
                 }
                 a += 1;
             }
+            Console.ReadLine();
+
+
+            for(int i = 1; i <= 20; i = i + 1)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();*/
+
+            //calcula la suma de los números entre n y m
+            /*//como yo lo hice, lo que se puede mejorar es crear una variable i en vez de modificar directamente la n.
+            int n1 = 15;
+            int m1 = 30;
+            int suma1;
+            for(suma = 0; n1 <= m1; n1 += 1)
+            {
+                suma1 += n1;
+            }
+            Console.WriteLine(suma);
+            Console.ReadLine();*/
+
+            //calcula la suma de los números impares entre n y m
+            int n = 15;
+            int m = 30;
+            int suma = 0;
+            for(int i = n; i <= m; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    suma = suma + i;
+                }
+            }
+            Console.WriteLine(suma);
             Console.ReadLine();
         }
     }
