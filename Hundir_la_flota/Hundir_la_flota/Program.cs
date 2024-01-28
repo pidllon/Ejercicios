016ad885d81ef95c218027952b458215bca09b5b
pidllon <iago.rodriguez.perez@ciclosmontecastelo.com>
2024-01-28 15:26:59 +0100

    ej44

--- Hundir_la_flota/Hundir_la_flota/Program.cs
+++ Hundir_la_flota/Hundir_la_flota/Program.cs
@@ -106,6 +106,57 @@ namespace Hundir_la_flota
                 {
                     Console.Clear();
                     Console.WriteLine("Ejercicio 44");
+                    int numFila;
+                    int[,] ej44 = { {5, 9, 2}, {4, 0, 3}, {1, 6, 8} };
+                    //A
+                    Console.WriteLine("Escoge una de las tres filas de la matriz e introduzcala:");
+                    numFila = int.Parse(Console.ReadLine());
+                    numFila--;
+                    Console.Clear();
+                    for(int contador = 0; contador < ej44.GetLength(1); contador++)
+                    {
+                        Console.Write(ej44[numFila, contador] + " ");
+                    }
+                    Console.ReadLine();
+                    //B
+                    for(int contador = 0; contador < ej44.GetLength(0); contador++)
+                    {
+                        ej44[contador, 2] = 0;
+                    }
+                    //C
+                    Console.Clear();
+                    int sumaValoresC = 0;
+                    for(int contador = 0; contador < ej44.GetLength(1); contador++)
+                    {
+                        sumaValoresC = sumaValoresC + ej44[0, contador];
+                    }
+                    Console.WriteLine("La suma de la primera fila es: " + sumaValoresC);
+                    //D
+                    int contadorCerosD = 0;
+                    for(int contador = 0; contador < ej44.GetLength(0); contador++)
+                    {
+                        if (ej44[contador, 1] == 0)
+                        {
+                            contadorCerosD++;
+                        }
+                    }
+                    Console.WriteLine("Número de ceros en la segunda columna: " + contadorCerosD);
+                    //E
+                    float sumaE, mediaE, contadorNumeroPosiciones;
+                    sumaE = 0;
+                    contadorNumeroPosiciones = 0;
+                    mediaE = 0;
+                    for(int contador = 0; contador < ej44.GetLength(0); contador++)
+                    {
+                        for(int contador2 = 0; contador2 < ej44.GetLength(1); contador2++)
+                        {
+                            sumaE = sumaE + ej44[contador, contador2];
+                            contadorNumeroPosiciones++;
+                        }
+                    }
+                    mediaE = sumaE / contadorNumeroPosiciones;
+                    Console.WriteLine($"La cuenta es: {sumaE} / {contadorNumeroPosiciones}");
+                    Console.WriteLine("La media de la matriz es de: " + mediaE);
                 }
                 else if(numEj == "46")
                 {
